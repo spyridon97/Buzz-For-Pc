04aa31e94af3e5524e4a5817c2989a66cb75d302
Spiros Tsalikis <spyridon97@hotmail.com>
2020-12-09 19:28:33 -0500

    Increase sleeping time

--- run_marathon.sh
+++ run_marathon.sh
@@ -2,7 +2,8 @@
 
 echo Create prefs directory
 xvfb-run --auto-servernum ./marathon/marathon &
-sleep 10
+echo Waiting for prefs creation
+sleep 60
 kill %1
 echo Run Tests
 xvfb-run --auto-servernum ./marathon/marathon -batch ./marathonTests/
