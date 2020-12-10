8fe56da2d56fd8636f4966c6ff2704a9d80249d4
Spiros Tsalikis <32746433+spyridon97@users.noreply.github.com>
2020-12-09 20:04:07 -0500

    Update run_marathon.sh

--- run_marathon.sh
+++ run_marathon.sh
@@ -4,11 +4,6 @@ echo Create prefs directory
 xvfb-run --auto-servernum ./marathon/marathon &
 echo Waiting for prefs creation
 while [ ! -f ~/.java/.userPrefs/net/prefs.xml ]; do sleep 1; done
-if [ -f ~/.java/.userPrefs/net/prefs.xml ]; then
-  echo true
-else
-  echo false
-fi
 kill %1
 echo Run Tests
 xvfb-run --auto-servernum ./marathon/marathon -batch ./marathonTests/
